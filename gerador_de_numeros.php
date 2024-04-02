8cbe6e8ee324ea85f697ae7e0bb397c828acb6ed
Igor Galdino <114758712+galdino013@users.noreply.github.com>
2024-04-02 18:46:33 -0300

    Update gerador_de_numeros.php
    Atualizado para melhoria de performance.

--- gerador_de_numeros.php
+++ gerador_de_numeros.php
@@ -1,27 +1,39 @@
 <?php
 
+/*
+Nome: Igor Galdino dos Santos
+Email: igor.galdino@zuukin.com
+Data: 02/04/2024
+*/
+
 // Define a quantidade de números gerados, 100 mil números, nesse caso.
-$Qntd_numeros = 100000; 
+$Qntd_numeros = 100000;
 
-// Abre o arquivo para escrita.
-$filename = 'numeros.txt';
-$file = fopen($filename, 'w');
+// Define o intervalo máximo para a geração de números aleatórios.
+$min = 1000000000;
+$max = 9999999999;
 
-// Gera e escreve números aleatórios de 10 dígitos no arquivo.
-for ($i = 0; $i < $Qntd_numeros; $i++) {
-    // Gera números aleatórios de 10 dígitos.
-    $number = mt_rand(1000000000, 9999999999); 
-    // Escreve os números no arquivo.
-    fwrite($file, $number . PHP_EOL); 
+// Cria um array para armazenar os números únicos.
+$unique_numbers = array();
+
+// Gera e armazena números aleatórios únicos.
+while (count($unique_numbers) < $Qntd_numeros) {
+    $number = mt_rand($min, $max);
+    $unique_numbers[$number] = true;
 }
 
-// Fecha o arquivo.
-fclose($file);
+// Converte os números em uma string separada por quebras de linha.
+$numbers_str = implode(PHP_EOL, array_keys($unique_numbers));
 
-// Verifica se o arquivo foi criado com sucesso e exibe a mensagem na tela do usúario.
+// Escreve a string de números no arquivo.
+$filename = 'numeros_desordenados.txt';
+file_put_contents($filename, $numbers_str);
+
+// Verifica se o arquivo foi criado com sucesso e exibe a mensagem na tela do usuário.
 if (file_exists($filename)) {
     echo "Arquivo gerado com sucesso: $filename";
     echo "<br> </br>";
 } else {
     echo "Houve um erro ao criar o arquivo.";
 }
+?>
